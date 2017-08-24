4e0ff0be7f4ec3b0df468b7f94a43af7ac7bc6d8
Rune Hjelsvold <rune.hjelsvold@ntnu.no>
2017-08-24 10:54:15 +0200

    ADDED: Template for implementing DBModel class.

--- Oblig1/src/model/DBModelTmpl.php
+++ Oblig1/src/model/DBModelTmpl.php
@@ -1,34 +1,79 @@
 <?php
-Interface IModel {
+include_once("IModel.php");
+include_once("Book.php");
+
+/** The Model is the class holding data about a collection of books. 
+ * @author Rune Hjelsvold
+ * @see http://php-html.net/tutorials/model-view-controller-in-php/ The tutorial code used as basis.
+ */
+class DBModel implements IModel
+{        
+    /**
+      * The PDO object for interfacing the database
+      *
+      */
+    protected $db = null;  
+    
+    /**
+	 * @throws PDOException
+     */
+    public function __construct($db = null)  
+    {  
+	    if ($db) 
+		{
+			$this->db = $db;
+		}
+		else
+		{
+            // Create PDO connection
+		}
+    }
+    
     /** Function returning the complete list of books in the collection. Books are
      * returned in order of id.
      * @return Book[] An array of book objects indexed and ordered by their id.
 	 * @throws PDOException
      */
-    public function getBookList();
+    public function getBookList()
+    {
+		$booklist = array();
+        return $booklist;
+    }
     
     /** Function retrieving information about a given book in the collection.
      * @param integer $id the id of the book to be retrieved
      * @return Book|null The book matching the $id exists in the collection; null otherwise.
 	 * @throws PDOException
      */
-    public function getBookById($id);
+    public function getBookById($id)
+    {
+		$book = null;
+        return $book;
+    }
     
     /** Adds a new book to the collection.
      * @param $book Book The book to be added - the id of the book will be set after successful insertion.
 	 * @throws PDOException
      */
-    public function addBook($book);
+    public function addBook(&$book)
+    {
+    }
 
     /** Modifies data related to a book in the collection.
      * @param $book Book The book data to be kept.
      * @todo Implement function using PDO and a real database.
      */
-    public function modifyBook($book);
+    public function modifyBook($book)
+    {
+    }
 
     /** Deletes data related to a book from the collection.
      * @param $id integer The id of the book that should be removed from the collection.
      */
-    public function deleteBook($id);
+    public function deleteBook($id)
+    {
+    }
+	
 }
+
 ?>
